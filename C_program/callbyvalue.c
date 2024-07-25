b191bb8ac8ddaa373ae31e7798c5bfc5bca4605c
Anjana-lad <iantanjanalad@gmail.com>
2024-07-25 16:50:20 +0530

    function

---
+++ C_program/callbyvalue.c
@@ -0,0 +1,16 @@
+//call by value :
+#include<stdio.h>
+int swap(int a,int b){
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("\n After swapping the value of a=%d  and b=%d ",a,b);
+}
+int main(){
+    int a,b;
+    printf("Enter values:");
+    scanf("%d %d",&a,&b);
+    printf("\n Before calling the function :\n a=%d \n b=%d",a,b);
+    swap(a,b);
+    printf("\n After calling function :\n a=%d \n b=%d",a,b);
+}
